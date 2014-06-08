85ebdcc5e9120f28fe0b29caf7e35a080c2e8485
Henry Kwok <henryk@nachocove.com>
2014-06-07 22:00:36 -0700

    Translate values for min, max, average fields to int.

--- Parse/scripts/parse.py
+++ Parse/scripts/parse.py
@@ -483,7 +483,7 @@ def main():
         if field in ['timestamp', 'createdAt', 'updatedAt', 'counter_start', 'counter_end']:
             if isinstance(sel, Parse.query.SelectorEqual):
                 sel.value = Parse.utc_datetime.UtcDateTime(sel.value)
-        elif field in ['count']:
+        elif field in ['count', 'min', 'max', 'average']:
             sel.value = int(sel.value)
 
     # Sanity check parameters
