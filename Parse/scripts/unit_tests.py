49c34f381154bb6c82855eac9f74d99cabb94369
Henry Kwok <henryk@nachocove.com>
2014-06-12 16:20:09 -0700

    Add new unit tests to the suite.

--- Parse/scripts/unit_tests.py
+++ Parse/scripts/unit_tests.py
@@ -1,11 +1,19 @@
 # This is the top-level harness for all unit test modules.
-# Add all new unit test module here
+#
+# To run all tests:
+#
+# python -m unittest -v unit_tests
+#
 
 import unittest
+
+# Add all new unit test module here.
 from html_elements_unit_tests import *
 from event_formatter_unit_tests import *
 from config_unit_tests import *
-from number_formatter import *
+from number_formatter_unit_tests import *
+from analytics.statistics_unit_tests import *
+from Parse.unit_tests import *
 
 if __name__ == '__main__':
     unittest.main()
