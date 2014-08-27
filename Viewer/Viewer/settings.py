b6417cb19218f81462a0baddad152312a799cea1
Henry Kwok <henryk@nachocove.com>
2014-08-27 10:33:25 -0700

    Add a logger setting for our app.

--- Viewer/Viewer/settings.py
+++ Viewer/Viewer/settings.py
@@ -79,3 +79,35 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)-8s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['console'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
+        'telemetry': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
+}
