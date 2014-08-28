bd46ebb4e56085babcd7535e6bb792f2e99075ac
Henry Kwok <henryk@nachocove.com>
2014-08-27 23:26:18 -0700

    Format all events in a table with color code. Handle UI events. (WBXML is not handle properly yet.) Add a summary table.

--- Viewer/Bugfix/views.py
+++ Viewer/Bugfix/views.py
@@ -13,6 +13,8 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 #from pytz import timezone
 import logging
+import re
+import cgi
 
 sys.path.append('../Parse/scripts')
 
@@ -47,6 +49,8 @@ def _parse_crash_report(junk):
         elif 'Date/Time' == key:
             timestamp = value.strip()
     if timestamp and device_id:
+        logger = logging.getLogger('telemetry').getChild('_parse_crash_report')
+        logger.debug('timestamp=%s, device_id=%s', timestamp, device_id)
         return {'timestamp': timestamp, 'device_id': device_id}
     return None
 
@@ -66,6 +70,8 @@ def _parse_error_report(junk):
         elif 'timestamp' == key:
             timestamp = value.strip()
     if timestamp and client:
+        logger = logging.getLogger('telemetry').getChild('_parse_error_report')
+        logger.debug('timestamp=%s, client=%d', timestamp, client)
         return {'timestamp': timestamp, 'client': client}
     return None
 
@@ -87,16 +93,21 @@ def login(request):
                 return HttpResponseRedirect('/')
             except Parse.exception.ParseException, e:
                 logger.error('fail to get session token - %s' % str(e))
+        else:
+            logger.warn('invalid form data')
     form = LoginForm()
     return render(request, 'login.html', {'form': form})
 
 
 def home(request):
+    logger = logging.getLogger('telemetry').getChild('home')
     if not 'session_token' in request.session:
+        logger.info('no session token. must log in first.')
         return HttpResponseRedirect('/login/')
     if request.method == 'POST':
         form = VectorForm(request.POST)
         if form.is_valid():
+            logger.debug('tele_paste=%s', form.cleaned_data['tele_paste'])
             loc = _parse_error_report(form.cleaned_data['tele_paste'])
             if loc is not None:
                 loc['span'] = str(default_span)
@@ -115,7 +126,12 @@ def home(request):
                     client = events[0]['client']
                     loc['client'] = client
                     loc['span'] = str(default_span)
+                    logger.debug('client=%(client)s, span=%(span)s', loc)
                     return HttpResponseRedirect("/bugfix/logs/%(client)s/%(timestamp)s/%(span)s/" % loc)
+            else:
+                logger.warn('unable to parse pasted info.')
+        else:
+            logger.warn('invalid form data')
     form = VectorForm()
     return render(request, 'home.html', {'form': form})
 
@@ -127,8 +143,12 @@ def _iso_z_format(date):
 
 
 def entry_page(request, client='', timestamp='', span=str(default_span)):
+    logger = logging.getLogger('telemetry').getChild('entry_page')
     if not 'session_token' in request.session:
+        logger.info('no session token. must log in first.')
         return HttpResponseRedirect('/login/')
+
+    logger.info('client=%s, timestamp=%s, span=%s', client, timestamp, span)
     span = int(span)
     client = str(client)
     center = dateutil.parser.parse(timestamp)
@@ -140,22 +160,112 @@ def entry_page(request, client='', timestamp='', span=str(default_span)):
     conn = Parse.connection.Connection(app_id=app_id, api_key=api_key,
                                        session_token=request.session['session_token'])
     query = Parse.query.Query()
-    query.limit = 5000
+    query.limit = 1000
     query.skip = 0
     query.add('client', Parse.query.SelectorEqual(client))
     #return HttpResponse(before)
     query.add('timestamp', Parse.query.SelectorGreaterThanEqual(Parse.utc_datetime.UtcDateTime(str(after))))
     query.add('timestamp', Parse.query.SelectorLessThan(Parse.utc_datetime.UtcDateTime(str(before))))
-    obj_list = Parse.query.Query.objects('Events', query, conn)[0]
+    logger.debug('query=%s', str(query.where()))
+    obj_list = list()
+    try:
+        obj_list = Parse.query.Query.objects('Events', query, conn)[0]
+        logger.info('%d objects found', len(obj_list))
+    except Parse.exception.ParseException, e:
+        logger.error('fail to query events - %s', str(e))
     iso_center = _iso_z_format(center)
     iso_go_earlier = _iso_z_format(go_earlier)
     iso_go_later = _iso_z_format(go_later)
-    ctxt = Context({'obj_list': obj_list,
-                    'client': client,
-                    'center': iso_center,
-                    'go_earlier': iso_go_earlier,
-                    'go_later': iso_go_later,
-                    'span': span,
-                    'zoom': span*2})
-    tpl = get_template('list.html')
-    return HttpResponse(tpl.render(ctxt))
+
+    # Just build a HTML page by hand
+    def add_ctrl_button(text, url):
+        return '  <td><a href=%s>%s</a></td>\n' % (url, text)
+
+    def ctrl_url(client_, time_, span_):
+        return '/bugfix/logs/%s/%s/%s/' % (client_, time_, span_)
+
+    def add_summary_row(desc, value):
+        return '  <tr><td>%s</td><td>%s</td></tr>\n' % (desc, value)
+
+    # Add 3 buttons
+    html = ''
+    html += '<table style="border-collapse: collapse" border="1" cellpadding="2"><tr>\n'
+    html += add_ctrl_button('Zoom out', ctrl_url(client, iso_center, span*2))
+    html += add_ctrl_button('Go back %d min' % span, ctrl_url(client, iso_go_earlier, span))
+    html += add_ctrl_button('Go forward %d min' % span, ctrl_url(client, iso_go_later, span))
+    html += '</tr></table><br/>\n'
+
+    # Add a summary table that describes some basic parameters
+    html += '<table style="border-collapse: collapse" border="1" cellpadding="2"><str>\n'
+    html += add_summary_row('Start Time (UTC)', after)
+    html += add_summary_row('Stop Time (UTC)', before)
+    html += add_summary_row('Client', client)
+    html += add_summary_row('# Events', len(obj_list))
+    if len(obj_list) > 0:
+        html += add_summary_row('OS Type', obj_list[0]['os_type'])
+        html += add_summary_row('OS Version', obj_list[0]['os_version'])
+        html += add_summary_row('Device Model', obj_list[0]['device_model'])
+        html += add_summary_row('Build Version', obj_list[0]['build_version'])
+    html += '</table><br/>\n'
+
+    if len(obj_list) > 0:
+        # Add the events
+        html += '<table style="border-collapse: collapse" border="1" cellpadding="2">\n'
+        html += '<tr><th>Date (UTC)</th><th>Time (UTC)</th><th>Event Type</th><th>Field</th><th>Value</th></tr>\n'
+        bg_colors = {'WARN': '#ffff99',
+                     'ERROR': 'pink',
+                     'WBXML_REQUEST': '#99ddff',
+                     'WBXML_RESPONSE': '#99ddff',
+                     'UI': 'lightgreen'}
+
+        for event in obj_list:
+            event_type = event['event_type']
+
+            def beautify_iso8601(time):
+                match = re.match('(?P<date>.+)T(?P<time>.+)Z', time)
+                assert match
+                return match.group('date'), match.group('time')
+
+            def row_header(event_type_):
+                # Set up row attributes
+                attrs = 'align="left" valign="top"'
+                if event_type in bg_colors:
+                    attrs += ' bgcolor="%s"' % bg_colors[event_type_]
+                header = '<tr %s>' % attrs
+                return header
+
+            def common_header(event_type_, iso, num_rows_):
+                (date, time) = beautify_iso8601(iso)
+                header = row_header(event_type_)
+                # Date, time, and event type
+                header += '<td rowspan="%d">%s</td><td rowspan="%d">%s</td><td rowspan="%d">%s</td>' % \
+                          (num_rows_, date, num_rows_, time, num_rows_, event_type_.replace('_', ' '))
+                return header
+
+            # Event type specific processing
+            if event_type in ['DEBUG', 'INFO', 'WARN', 'ERROR']:
+                html += common_header(event_type, event['timestamp']['iso'], 1)
+                html += '<td>message</td><td>%s</td>' % cgi.escape(event['message'])
+            elif event_type in ['WBXML_REQUEST', 'WBXML_RESPONSE']:
+                html += common_header(event_type, event['timestamp']['iso'], 1)
+                html += '<td>wbxml</td><td>%s</td>' % cgi.escape(event['wbxml']['base64'])
+            elif event_type == 'UI':
+                num_rows = 2  # for ui_type, ui_object
+                if 'ui_string' in event:
+                    num_rows += 1
+                if 'ui_integer' in event:
+                    num_rows += 1
+                tr = row_header(event_type)
+                html += common_header(event_type, event['timestamp']['iso'], num_rows)
+                html += '<td>ui_type</td><td>%s</td></tr>\n' % event['ui_type']
+                html += tr + '<td>ui_object</td><td>%s</td>' % event['ui_object']
+                if 'ui_string' in event:
+                    html += '</tr>\n' + tr + '<td>ui_string</td><td>%s</td>' % event['ui_string']
+                if 'ui_integer' in event:
+                    html += '</tr>\n' + tr + '<td>ui_integer</td><td>%s</td>' % event['ui_integer']
+            else:
+                logger.warn('unknown handled event type %s', event_type)
+                html += common_header(event_type, event['timestamp']['iso'], 1)
+            html += '</tr>\n'
+        html += '</table>\n'
+    return HttpResponse(html)
