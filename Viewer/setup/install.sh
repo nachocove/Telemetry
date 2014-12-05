9116e9e9f083fea116d700b02f9660c66e135560
Jan Vilhuber <jan@vilhuber.com>
2014-12-05 10:17:00 -0700

    fix install script

--- Viewer/setup/install.sh
+++ Viewer/setup/install.sh
@@ -17,14 +17,21 @@ fi
 which curl > /dev/null
 check_exit_code "cannot find curl."
 
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-check_exit_code "cannot download get-pip.py."
+echo "Downloading and installing/updating easy_install"
+# get and install easy_install/setuptools
+curl -s https://bootstrap.pypa.io/ez_setup.py -o - | python > /dev/null
+check_exit_code "cannot download/run ez_setup.py."
 
-python get-pip.py
-check_exit_code "cannot install pip."
-rm -f get-pip.py
+# use easy_install to install/update pip
+easy_install -q -U pip
+check_exit_code "cannot easy_install pip"
 
-pip install Django
+echo "Installing boto"
+pip -q install boto
+check_exit_code "cannot install boto."
+
+echo "Installing Django"
+pip -q install Django
 check_exit_code "cannot install Django."
 
 django_version=`python -c "import django ; print django.get_version()"`
