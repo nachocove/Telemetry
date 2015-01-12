4da6b36721425bfe9d0f50b17accb57e9cb5bb1a
Jan Vilhuber <jan@vilhuber.com>
2015-01-12 12:44:46 -0700

    make the cron file a template

--- config/install_cron.sh
+++ config/install_cron.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
+script_name=`basename $0`
 config_path=`dirname $0`
 repo_path=`dirname $config_path`
-#echo "Telemetry working copy at: $repo_path"
 scripts_path=$repo_path/scripts
-#echo "Telemetry scripts at: $scripts_path"
+
+if [ -z "$1" ] ; then
+  echo "ERROR: $script_name <email config>"
+  exit 1
+fi
+
+if [ ! -f $config_path/$1 ] ; then
+   echo "ERROR: Email config file $config_path/$1 does not exist."
+   exit 1
+fi
+
 if [ ! -d $scripts_path ]
 then
   echo "ERROR: The Telemetry working copy seems to be incomplete. Cannot find $scripts_path"
@@ -12,4 +22,4 @@ then
 fi
 
 echo "Copying cron definitions from $scripts_path/cron/ to /etc/cron.d/"
-sudo cp $scripts_path/cron/nacho-cove /etc/cron.d/
+sudo m4 -DCONFIG_DIR=$config_path -DEMAIL_CFG=$1 $scripts_path/cron/nacho-cove.template > /etc/cron.d/nacho-cove
