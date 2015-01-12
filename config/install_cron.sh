6ec5f0e92b060454fa890eae983f1c2845550b6e
Jan Vilhuber <jan@vilhuber.com>
2015-01-12 10:56:07 -0700

    remove overly verbose debugs

--- config/install_cron.sh
+++ config/install_cron.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-repo_path=`dirname $PWD`
-echo "Telemetry working copy at: $repo_path"
+config_path=`dirname $0`
+repo_path=`dirname $config_path`
+#echo "Telemetry working copy at: $repo_path"
 scripts_path=$repo_path/scripts
-echo "Telemetry scripts at: $scripts_path"
+#echo "Telemetry scripts at: $scripts_path"
 if [ ! -d $scripts_path ]
 then
   echo "ERROR: The Telemetry working copy seems to be incomplete. Cannot find $scripts_path"
   exit 1
-else
-  echo "$scripts_path found."
 fi
 
+echo "Copying cron definitions from $scripts_path/cron/ to /etc/cron.d/"
 sudo cp $scripts_path/cron/nacho-cove /etc/cron.d/
