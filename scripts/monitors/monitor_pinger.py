ffa4bf07d4ac99ff5d609f9208e4669351bfbc57
Jan Vilhuber <jan@vilhuber.com>
2015-04-17 09:38:15 -0600

    fix crash when an old record without a session field is seen

--- scripts/monitors/monitor_pinger.py
+++ scripts/monitors/monitor_pinger.py
@@ -92,7 +92,7 @@ class MonitorPingerPushMessages(MonitorPinger):
                 if 'PerformFetch called' in ev['message']:
                     if not push_received_event:
                         perform_fetch = perform_fetch or ev
-                elif 'Got remote notification' in ev['message'] and "session = %s" % push['session'] in ev['message']:
+                elif 'Got remote notification' in ev['message'] and "session" in push and "session = %s" % push['session'] in ev['message']:
                     push_received_event = ev
                     break
 
