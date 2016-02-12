3b4bc3b6b7fda474078d926abafaa415a77af31e
Jan Vilhuber <jan@vilhuber.com>
2016-02-12 11:50:05 -0700

    get the project from the config, not hardcoded

--- scripts/DnldAnalysis.py
+++ scripts/DnldAnalysis.py
@@ -17,8 +17,8 @@ def json_config(file_name):
 
 
 sql1_where_options = ["message like 'Starting DnldEmailBodyCmd%%McPending%%'", ]
-sql1 = "select distinct split_part(message, '/', 2),device_id,message from alpha_nm_log where %s order by timestamped;"
-sql2 = "select timestamped,device_id,message from alpha_nm_log where %s order by timestamped;"
+sql1 = "select distinct split_part(message, '/', 2),device_id,message from %(project)s_nm_log where %(where)s order by timestamped;"
+sql2 = "select timestamped,device_id,message from %(project)s_nm_log where %s order by timestamped;"
 sql2_where_options = ["device_id='%(device)s'"]
 sql2_message_or_options = ["message like 'McPending%%/%(guid)s/EmailBodyDownload%%'",
                            ]
@@ -49,6 +49,7 @@ def main():
         sys.exit(1)
 
     config = args.config
+    project = config['general_config']['project']
 
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG if args.debug else logging.INFO)
@@ -69,10 +70,10 @@ def main():
     if args.device:
         sql1_where_options.append("device_id='%(device)s'")
 
-    q = (sql1 % " and ".join(sql1_where_options)) % {'after': args.after,
-                                                     'before': args.before,
-                                                     'device': args.device,
-                                                    }
+    q = (sql1 % {'project': project, 'where': " and ".join(sql1_where_options)}) % {'after': args.after,
+                                                                                    'before': args.before,
+                                                                                    'device': args.device,
+                                                                                    }
     logger.debug("Query: \"%s\"", q)
     rows = select(logger, cursor, q)
     if rows:
@@ -87,10 +88,11 @@ def main():
             else:
                 sql2_where_options.extend(sql2_message_or_options)
 
-            q = (sql2 % " and ".join(sql2_where_options)) % {'after': args.after,
-                                                             'before': args.before,
-                                                             'guid': guid,
-                                                             'device': device}
+            q = (sql2 % {'project': project, 'where': " and ".join(sql2_where_options)}) % {'after': args.after,
+                                                                                            'before': args.before,
+                                                                                            'guid': guid,
+                                                                                            'device': device,
+                                                                                            }
             logger.debug("   Query: \"%s\"", q)
             guid_rows = select(logger, cursor1, q)
             started = None
@@ -123,6 +125,8 @@ def main():
             if args.verbose or args.all_results or "Success" not in result or took > timedelta(seconds=args.seconds):
                 d['items'] = "\n".join(items) if args.verbose else ""
                 logger.info("%(device)s: Download %(guid)s took: %(took)s (started %(started)s, finished %(finished)s), results: %(results)s\n%(items)s" % d)
+    else:
+        logger.info("No rows found")
 
     exit(0)
 
