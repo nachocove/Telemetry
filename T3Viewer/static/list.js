066916794d90b82406ed75cd3af89fee4e18dbc8
Jan Vilhuber <jan@vilhuber.com>
2016-03-30 12:28:36 -0600

    add missing comma

--- T3Viewer/static/list.js
+++ T3Viewer/static/list.js
@@ -337,7 +337,7 @@ function refreshEvents() {
         var current_timestamp = new Date(event.timestamp).getTime()/1000;
         if (last_timestamp != null) {
             if (current_timestamp - last_timestamp > 20) {
-                row = getDummyRow("WARN", "EXCESSIVE_TIME" "SECONDS SINCE LAST EVENT: " + (current_timestamp - last_timestamp) + "<br/>");
+                row = getDummyRow("WARN", "EXCESSIVE_TIME", "SECONDS SINCE LAST EVENT: " + (current_timestamp - last_timestamp) + "<br/>");
                 table.appendChild(row);
             }
         }
